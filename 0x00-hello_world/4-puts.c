a2a11dd65632be696de2d501a03cf4061cc558db
JanviaveTharwat <janviavetharwat@gmail.com>
2023-03-16 12:28:49 -0700

    4.5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  main will print "Programming is like building a multilingual puzzle" by using puts function
+ *  it wiill return zero
+ */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
