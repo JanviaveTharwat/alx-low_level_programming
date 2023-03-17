424b541baf2064b38ef240f8fef2bf2fa682d55e
JanviaveTharwat <janviavetharwat@gmail.com>
2023-03-17 13:59:36 -0700

    5.6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ * main -prints all single digit numbers 10 from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar((i % 10) + '0');
+	}
+	printf('\n');
+	return (0);
+}
