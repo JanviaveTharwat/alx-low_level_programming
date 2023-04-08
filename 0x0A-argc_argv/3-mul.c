4920e30199c0359500e1120ff23bd11050febc4b
JanviaveTharwat <janviavetharwat@gmail.com>
2023-04-08 13:15:50 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+/**
+ * main - multiplies two numbers.
+ * @argc: argument
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int a, b;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
+
+		return (0);
+	}
+}
