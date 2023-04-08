d3e616eb3e0d671e540afde1571feacbab5a2b33
JanviaveTharwat <janviavetharwat@gmail.com>
2023-04-08 13:25:09 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,17 +10,18 @@
 
 int main(int argc, char *argv[])
 {
-	int a;
+	int a, b, c;
 
-	if (argc != 3)
+	if (argc <= 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		a = (atoi(argv[1]) * atoi(argv[2]));
-		printf("%d\n", a);
-	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = a * b;
+
+	printf("%d\n", c);
 	return (0);
 }
