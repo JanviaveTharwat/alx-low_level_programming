e39bfb58c39bc2671628d15a4cb64e78f6de1f33
JanviaveTharwat <janviavetharwat@gmail.com>
2023-04-08 12:58:57 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints  numbers.
+ * @argc: argument
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+	printf("%s\n", argv - 1);
+	return (0);
+}
