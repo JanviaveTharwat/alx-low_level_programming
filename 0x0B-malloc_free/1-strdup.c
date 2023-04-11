c14d956133f68321a61c29729964c7dccba6effa
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-04-11 12:15:38 +0200

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,23 +14,19 @@ char *new;
 int a = 0, b = 0;
 
 if (str == NULL)
-{
 return (NULL);
-}
+
 for (; str[b] != '\0'; b++)
-{
 new = malloc(b * sizeof(*str) + 1);
-}
+
 if (new == 0)
 {
 return (NULL);
 }
 else
 {
-for (; a < b ; a++)
-{
+for (; a < b; a++)
 new[a] = str[a];
-}
 }
 return (new);
 }
