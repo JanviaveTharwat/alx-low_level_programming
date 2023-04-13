edec083fe5d9095c92a46f1c229b72be63551d9f
JanviaveTharwat <janviavetharwat@gmail.com>
2023-04-13 13:09:43 -0700

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,14 +12,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *pt;
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	pt = malloc(nmemb * size);
 	if (pt != NULL)
 	{
-		for (i = 0; i < (nmemb * size); i++)
+		for (; i < (nmemb * size); i++)
 			pt[i] = 0;
 		return (pt);
 	}
