6a65f10631d51bd643fe7e147fd3f90682742e8d
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-04-14 04:28:16 +0200

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -59,9 +59,7 @@ int len1, len2, len, i, carry, digit1, digit2, digit_mult, *result, a = 0;
 s1 = argv[1], s2 = argv[2];
 if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 errors();
-len1 = _strlen(s1);
-len2 = _strlen(s2);
-len = len1 + len2 + 1;
+len1 = _strlen(s1), len2 = _strlen(s2), len = len1 + len2 + 1;
 result = malloc(sizeof(int) * len);
 if (!result)
 return (1);
@@ -69,15 +67,11 @@ for (i = 0; i <= len1 + len2; i++)
 result[i] = 0;
 for (len1 = len1 - 1; len1 >= 0; len1--)
 {
-digit1 = s1[len1] - '0';
-carry = 0;
+digit1 = s1[len1] - '0', carry = 0;
 for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 {
-digit2 = s2[len2] - '0';
-digit_mult = digit1 *digit2;
-carry += result[len1 + len2 + 1] + digit_mult;
-result[len1 + len2 + 1] = carry % 10;
-carry /= 10;
+digit2 = s2[len2] - '0', digit_mult = digit1 *digit2, carry += result[len1 + len2 + 1] + digit_mult;
+result[len1 + len2 + 1] = carry % 10, carry /= 10;
 }
 if (carry > 0)
 result[len1 + len2 + 1] += carry;
