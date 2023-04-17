2b66a694bfb36295477c6bd524d0801709dbf8ec
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-04-17 17:59:11 +0200

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -25,7 +25,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-int _strlen(char *str);
-char *_strcopy(char *dest, char *src);
+int _strlen(char *long);
+char *_strcopy(char *store, char *src);
 
 #endif
