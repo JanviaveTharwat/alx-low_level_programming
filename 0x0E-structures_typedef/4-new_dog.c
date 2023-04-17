da0beace516fcdd3cb8e4de8afa93135d8e7019c
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-04-17 18:21:59 +0200

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,6 +5,42 @@
 
 
 /**
+ * _strlen -find length
+ * @str: The string length
+ * Return: The length of the string
+ */
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
+}
+
+/**
+ * _strcopy - Copies a string
+ * @dest: past string
+ * @src: string copy
+ * Return: The pointer to dest.
+ */
+
+
+char *_strcopy(char *dest, char *src)
+{
+	int index = 0;
+
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
+
+	dest[index] = '\0';
+
+	return (dest);
+}
+
+/**
  * new_dog - new dog
  * @name: name's dog
  * @age: age's dog
@@ -15,40 +51,33 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int d = 0, g = 0, o;
-dog_t *puppy;
+	dog_t *dog;
 
-while (name[d] != '\0')
-d++;
+	if (!name || age < 0 || !owner)
+		return (NULL);
 
-while (owner[g] != '\0')
-g++;
+	dog = (dog_t *)malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
 
-puppy = malloc(sizeof(dog_t));
-if (puppy == NULL)
-{
-free(puppy);
-return (NULL);
-}
-puppy->name = malloc(d *sizeof(puppy->name));
-if (puppy->name == NULL)
-{
-free(puppy->name);
-free(puppy);
-return (NULL);
-}
-for (o = 0; o <= d; o++)
-puppy->name[o] = name[o];
-puppy->age = age;
-puppy->owner = malloc(g *sizeof(puppy->owner));
-if (puppy->owner == NULL)
-{
-free(puppy->owner);
-free(puppy->name);
-free(puppy);
-return (NULL);
-}
-for (o = 0; o <= g; o++)
-puppy->owner[o] = owner[o];
-return (puppy);
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if ((*dog).name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	dog->name = _strcopy(dog->name, name);
+	dog->age = age;
+	dog->owner = _strcopy(dog->owner, owner);
+
+	return (dog);
 }
