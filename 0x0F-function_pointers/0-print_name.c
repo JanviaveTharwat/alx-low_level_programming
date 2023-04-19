994ab8988f19442e53626e8cff510b51cbb878a0
JanviaveTharwat <janviavetharwat@gmail.com>
2023-04-19 12:25:18 -0700

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+
+/**
+ * print_name - prints a name
+ * @name: name
+ * @f: pointer function
+ * Return : void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+		f(name);
+}
