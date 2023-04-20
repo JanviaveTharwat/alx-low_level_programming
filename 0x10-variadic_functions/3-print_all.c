5a861213bdf46c5338acd7b3a8171d16fafd7a72
JanviaveTharwat <janviavetharwat@gmail.com>
2023-04-20 11:49:09 -0700

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,95 +2,47 @@
 
 
 /**
- * print_char - print a char
- * @arg: a list of argument
- * Return: void
+ * print_all - prints anything
+ * @format: list types
+ * Return :void
  */
-
-
-void print_char(va_list arg)
-{
-	char abc = va_arg(arg, int);
-
-	printf("%c", abc);
-}
-
-/**
- * print_int - prints integer
- * @arg: a list of argument
- * Return: void
- */
-
-void print_int(va_list arg)
-{
-	int num = va_arg(arg, int);
-
-	printf("%d", num);
-}
-
-/**
- *  print_float - print a float
- *  @arg: a list of argument
- *  Return: void
- */
-
-void print_float(va_list arg)
-{
-	float f = va_arg(arg, double);
-
-	printf("%f", f);
-}
-
-/**
- * print_string - print a string
- * @arg: a list of argument
- * Return: void
- */
-
-void print_string(va_list arg)
-{
-	char *abc = va_arg(arg, char *);
-
-	if (abc == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", abc);
-}
-
-/**
- * print_all - a function that prints anything
- * @format: A string of character
- * Return: void
- */
-
 void print_all(const char * const format, ...)
 {
+	int i = 0;
+	char *sr, *sp = "";
+
 	va_list ls;
-	int i = 0, j;
-	char *sp = "";
-	print_funcs fp = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_string}
-	};
 
 	va_start(ls, format);
 
-	while (format && format[i])
+	if (format)
 	{
-		j = 0;
-		while (j < 4 && (format[i] != *(fp.symbol)))
-			j++;
-		if (j < 4)
+		while (format[i])
 		{
-			printf("%s", sp);
-			fp.print_funcs(ls);
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sp, va_arg(ls, int));
+					break;
+				case 'i':
+					printf("%s%d", sp, va_arg(ls, int));
+					break;
+				case 'f':
+					printf("%s%f", sp, va_arg(ls, double));
+					break;
+				case 's':
+					sr = va_arg(ls, char *);
+					if (!sr)
+						sr = "(nil)";
+					printf("%s%s", sp, sr);
+					break;
+				default:
+					i++;
+					continue;
+			}
 			sp = ", ";
+			i++;
 		}
-		i++;
 	}
 	printf("\n");
 	va_end(ls);
