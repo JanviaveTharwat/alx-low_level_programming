918c858830620a45f94a9d0d6d8f9d115a92ea02
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-05-02 18:56:39 +0300

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,24 +1,24 @@
 #include "lists.h"
 
 /**
- * free_listint2 - clear a linked list
- * @head: pointer
- * Return: void
- */
+* free_listint2 - frees a linked list
+* @head: pointer
+* Return: void
+*/
 
 void free_listint2(listint_t **head)
 {
-	listint_t *ab;
-	listint_t **n = head;
+listint_t *cursor;
+listint_t **temp = head;
 
-	if (n != NULL)
-	{
-		while (*head != NULL)
-		{
-			ab = *head;
-			free(ab);
-			*head = (*head)->next;
-		}
-		*n = NULL;
-	}
+if (temp != NULL)
+{
+while (*head != NULL)
+{
+cursor = *head;
+free(cursor);
+*head = (*head)->next;
+}
+*temp = NULL;
+}
 }
