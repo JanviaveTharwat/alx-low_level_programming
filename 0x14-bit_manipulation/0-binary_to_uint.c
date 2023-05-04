5d89d8f269b5ab65aaf04f8cf680245e7fec20fb
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-05-04 13:34:33 +0300

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,19 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0;
-	unsigned int j = 0;
-
-
+	int i;
+	unsigned int j;
+	
+	j = 0;
+	
 	if (!b)
 		return (0);
-	for (; b[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
-	for (; b[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		j <<= 1;
 		if (b[i] == '1')
