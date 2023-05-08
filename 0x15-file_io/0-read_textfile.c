a82c0f8a1d1d7eff7de8827e71e760a7d290324c
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-05-08 16:35:34 +0300

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * read_textfile - Readsand prints
@@ -13,10 +12,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t  o, r, w;
 	char *pt;
 	
-	o = open(filename, O_RDONLY);
-	r = read(o, pt, letters);
-	w = write(STDOUT_FILENO, pt, r);
-	
 	if (filename == NULL)
 		return (0);
 
@@ -24,7 +19,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (pt == NULL)
 		return (0);
 
-	
+	o = open(filename, O_RDONLY);
+	r = read(o, pt, letters);
+	w = write(STDOUT_FILENO, pt, r);
 
 	if (o == -1 ||
 			r == -1 ||
