abc10e4ec797bfd253df385b05daaa1b9edd89fc
janviave tharwat <janviavetharwat@gmail.com>
2023-05-08 07:52:09 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * copy- copies the content of a file to another file
+ * main- copies the content of a file to another file
  * @argc: number of arguments passed
  * @argv: double pointer
  * Return: the actual number of letters it could read and print
  */
 
-int copy(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int cp1, cp2, n;
+	int f1, f2, n;
 	char buf[1024];
 
 	if (argc != 3)
@@ -17,16 +17,16 @@ int copy(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	cp1 = open(argv[1], O_RDONLY);
-	if (cp1 == -1)
+	f1 = open(argv[1], O_RDONLY);
+	if (f1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	cp2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	while ((n = read(cp1, buf, 1024)) > 0)
+	f2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	while ((n = read(f1, buf, 1024)) > 0)
 	{
-		if (write(cp2, buf, n) != n || cp2 == -1)
+		if (write(f2, buf, n) != n || f2 == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
@@ -37,14 +37,14 @@ int copy(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(cp1) < 0)
+	if (close(f1) < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cp1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
 		exit(100);
 	}
-	if (close(cp2) < 0)
+	if (close(f2) < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cp2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2);
 		exit(100);
 	}
 	return (0);
