9c93ea8475255221550c9ffbeb1276e9011fad26
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-09-29 11:53:36 +0200

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -20,5 +20,7 @@ void print_data(unsigned char *e_ident)
 void print_osabi(unsigned char *e_ident);
 void print_abi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+
 
 #endif
