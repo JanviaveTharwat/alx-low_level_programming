ec5ffba34214a87f199238ead58203fb92dba61f
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-10-10 06:46:26 +0200

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,10 +14,10 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
-	
+
 	if (!array || size == 0)
 		return (-1);
-	
+
 	while (i < size)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
