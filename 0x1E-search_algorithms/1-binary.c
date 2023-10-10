bfe9d89544f684521c41254cb4c4a04bdb5e31c3
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-10-10 06:48:19 +0200

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,7 +11,7 @@
 int binary_search(int *array, size_t size, int value)
 {
 	size_t i, left, right;
-	
+
 	if (array == NULL)
 		return (-1);
 
@@ -21,7 +21,7 @@ int binary_search(int *array, size_t size, int value)
 		for (i = left; i < right; i++)
 			printf("%d, ", array[i]);
 		printf("%d\n", array[i]);
-	
+
 		i = left + (right - left) / 2;
 		if (array[i] == value)
 			return (i);
